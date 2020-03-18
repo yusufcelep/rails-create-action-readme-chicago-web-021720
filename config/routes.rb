42fa20b547a8621d39a2891a838f0b0aac74cadf
github username <github email address>
2020-03-18 14:56:07 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, only: [:index, :new, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
